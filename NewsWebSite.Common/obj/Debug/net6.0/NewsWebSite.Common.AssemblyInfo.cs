8a38bb5a3c3f7387d84f72e8d186a1946fd47825
mehdi <mehdipourhashemi1@gmail.com>
2025-03-15 10:32:02 +0330

    th

--- NewsWebSite.Common/obj/Debug/net6.0/NewsWebSite.Common.AssemblyInfo.cs
+++ NewsWebSite.Common/obj/Debug/net6.0/NewsWebSite.Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewsWebSite.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c884711922b47a3c08496f7dc291da6e489dcb02")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewsWebSite.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewsWebSite.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
